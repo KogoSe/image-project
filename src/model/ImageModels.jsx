4f66a2b117c8ef9a1b3727129cc08d31bd5d631e
KogoSe <youkoy1890@gmail.com>
2021-11-30 19:55:05 +0700

    can import image

--- src/model/ImageModels.jsx
+++ src/model/ImageModels.jsx
@@ -1,29 +1,40 @@
+import dog1 from '../image/dog/dog1.jpg'
+import dog2 from '../image/dog/dog2.jpg'
+import dog3 from '../image/dog/dog3.jpg'
+import dog4 from '../image/dog/dog4.jpg'
+import dog5 from '../image/dog/dog5.jpg'
+
+import cat1 from '../image/cat/cat1.png'
+import cat2 from '../image/cat/cat2.jpg'
+import cat3 from '../image/cat/cat3.jpg'
+import cat4 from '../image/cat/cat4.jpg'
+import cat5 from '../image/cat/cat5.jpg'
 
 export const IMAGES_DOG = [
     {
         id:1,
         name:"DOG1",
-        imagePath: "src/image/dog/dog1.jpg"
+        imagePath: dog1
     },
     {
         id:2,
         name:"DOG2",
-        imagePath: "src/image/dog/dog2.jpg"
+        imagePath: dog2
     },
     {
         id:3,
         name:"DOG3",
-        imagePath: "src/image/dog/dog3.jpg"
+        imagePath: dog3
     },
     {
         id:4,
         name:"DOG4",
-        imagePath: "src/image/dog/dog4.jpg"
+        imagePath: dog4
     },
     {
         id:5,
         name:"DOG5",
-        imagePath: "src/image/dog/dog5.jpg"
+        imagePath: dog5
     }
 ]
 
@@ -31,26 +42,26 @@ export const IMAGES_CAT = [
     {
         id:1,
         name:"CAT1",
-        imagePath: "src/image/cat/cat1.png"
+        imagePath: cat1
     },
     {
         id:2,
         name:"CAT2",
-        imagePath: "src/image/cat/cat2.jpg"
+        imagePath: cat2
     },
     {
         id:3,
         name:"CAT3",
-        imagePath: "src/image/cat/cat3.jpg"
+        imagePath: cat3
     },
     {
         id:4,
         name:"CAT4",
-        imagePath: "src/image/cat/cat4.jpg"
+        imagePath: cat4
     },
     {
         id:5,
         name:"CAT5",
-        imagePath: "src/image/cat/cat5.jpg"
+        imagePath: cat5
     }
 ]
